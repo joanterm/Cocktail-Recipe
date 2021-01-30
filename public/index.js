ff2536f1fda01250d28e44e4a00727ac30aeaa11
joanterm <jo_joanarg@yahoo.com>
2021-01-29 20:11:51 -0500

    TEST. Needs fixing

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+//-----------------------------RANDOM RECIPE PART OF THE PROJECT //THIS ALL WORKS FINE
 const closeBtn = document.querySelector("#close")
 const randomBtn = document.querySelector("#open-random")
 const overlay = document.querySelector("#overlay")
@@ -6,16 +7,18 @@ const randomDrinkImage = document.querySelector("#random-drink-image")
 const randomDrinkIngredient = document.querySelector("#random-drink-ingr")
 const randomDrinkRecipe = document.querySelector("#random-drink-recipe")
 
-
+//CLOSES THE OVERLAY AND FIRES RANDOMRECIPE FUNCTION (SO THAT IT'S ALREADY LOADED ONCE RANDOMBTN GETS CLICKED)
 closeBtn.addEventListener("click", function() {
-    overlay.classList.add("hidden")
+    overlay.classList.add("hidden") 
     overlay.classList.remove("flex")
     randomRecipe()
 })
 
+//DISPLAYS THE OVERLAY (RANDOM RECIPE FUNCTION IS ALREADY LOADED)
 randomBtn.addEventListener("click", function() {
     overlay.classList.remove("hidden")
     overlay.classList.add("flex")
+
 })
 
 
@@ -63,16 +66,61 @@ randomRecipe();
 
 
 
+//-----------------------------DROPDOWN SELECT LIST PART OF THE PROJECT, BUGS THAT NEED FIXING
+
+//this code will make sure only the selected option from the menu (such as === Margatita) displays getMargarita() function (I found parts of this code on stackoverflow)
+let submitBtn = document.querySelector("#submit")
+//this is the dropdown portion of the project
+submitBtn.addEventListener("click", function() {
+    var dd = document.getElementById("select");
+    console.log(dd.options[dd.selectedIndex].innerHTML);
+        if(dd.options[dd.selectedIndex].innerHTML === "Margarita") {
+            overlay.classList.remove("hidden")
+            overlay.classList.add("flex")
+            getMargarita()    //this is supposed to open a modal with margarita and it does but the width of the modal changes for some reason
+        }
+})
 
 
-//DROPDOWN MENU
-const select = document.querySelector("#select")
-let arrays = ["Cosmopolitan", "Manhattan", "Margarita"]
 
-for(let i=0; i<arrays.length; i++) {
-    let option = document.createElement("option")
-    let txt=document.createTextNode(arrays[i])
-    option.appendChild(txt)
-    //option.setAttribute("value", arrays[i])
-    select.insertBefore(option, select.lastChild)
+//I copied this function from getRandom() function, only changed values to match margarita's API
+async function getMargarita() {
+    const margaritaPromise = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
+
+    if(margaritaPromise.ok) {
+        const margaritaParse = await margaritaPromise.json();
+        let margarita = margaritaParse.drinks[0]
+        console.log(margarita)
+        randomDrinkName.innerHTML = margarita.strDrink
+        randomDrinkImage.src = margarita.strDrinkThumb
+        randomDrinkRecipe.innerHTML = margarita.strInstructions
+        
+        let margaritaIngr = [];
+        for(let i=1; i<=15; i++) {
+
+            if(margarita[`strIngredient${i}`]  === null) {
+                break;
+            }
+            
+            if(margarita[`strMeasure${i}`]  === null && margarita[`strIngredient${i}`] !== null) {
+                margaritaIngr.push(margarita[`strIngredient${i}`])
+            }
+
+            if(margarita[`strMeasure${i}`]  !== null && margarita[`strIngredient${i}`] !== null) {
+                margaritaIngr.push(margarita[`strMeasure${i}`] + " " + margarita[`strIngredient${i}`])
+            }
+
+
+
+            var margaritastr = "<ul>"
+            margaritaIngr.forEach(function(element) {
+                margaritastr += "<li>" + element + "</li>"
+            })
+            margaritastr += "</ul>"
+            randomDrinkIngredient.innerHTML = margaritastr
+
+        }
+    }
+
 }
+getMargarita() //for some reason when getMargarita() API loads second, when I click "RANDOM RECIPE" button it displays this function even though it has no access to it
